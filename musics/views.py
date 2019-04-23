4ee78822970badfe22bb56296831cc6ca744e6cc
hoyoung2176 <hoyoung2176@gamil.com>
2019-04-23 06:35:38 +0000

    01_artist list&& artist detail && comment create

--- musics/views.py
+++ musics/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import MusicSerializers
-from .models import Music
+from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer
+from .models import Music, Artist, Comment
 
 
 # Create your views here.
@@ -17,5 +17,31 @@ from .models import Music
 @api_view(['GET'])
 def music_list(request):
     musics = Music.objects.all()
-    seializer = MusicSerializers(musics, many = True)
+    serializer = MusicSerializer(musics, many = True)
     return Response(serializer.data)
+    
+@api_view(['GET'])
+def music_detail(request, music_pk):
+    music = get_object_or_404(Music, pk= music_pk)
+    serializer = MusicSerializer(music)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+def artist_list(request):
+    artists = Artist.objects.all()
+    serializer = ArtistSerializer(artists, many=True)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+def artist_detail(request, artist_pk):
+    artist = get_object_or_404(Artist, pk= artist_pk)
+    serializer = ArtistDetailSerializer(artist)
+    return Response(serializer.data)
+    
+@api_view(['POST'])
+def comment_create(request, music_pk):
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(music_id=music_pk)
+        return Response(serializer.data)
+    
